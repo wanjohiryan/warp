131281da8785155b40c5227438982b2c050a2017
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-01-23 19:29:54 +0300

    create automated releases (#12)

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/kixelated/invoker"
 	"github.com/wanjohiryan/warp/internal/warp"
 )
+
+type Config struct {
+	address  string
+	certFile string
+	keyFile  string
+	logDir   string
+	media    string
+}
 
 func main() {
 	err := run(context.Background())
@@ -19,29 +28,23 @@ func main() {
 }
 
 func run(ctx context.Context) (err error) {
-	addr := flag.String("addr", ":8080", "HTTPS server address")
-	cert := flag.String("tls-cert", "./certs/localhost.crt", "TLS certificate file path")
-	key := flag.String("tls-key", "./certs/localhost.key", "TLS certificate file path")
-	logDir := flag.String("log-dir", "", "logs will be written to the provided directory")
 
-	dash := flag.String("dash", "./media/playlist.mpd", "DASH playlist path")
+	myConfig := getConfig()
 
-	flag.Parse()
-
-	media, err := warp.NewMedia(*dash)
+	media, err := warp.NewMedia(myConfig.media)
 	if err != nil {
 		return fmt.Errorf("failed to open media: %w", err)
 	}
 
-	tlsCert, err := tls.LoadX509KeyPair(*cert, *key)
+	tlsCert, err := tls.LoadX509KeyPair(myConfig.certFile, myConfig.keyFile)
 	if err != nil {
 		return fmt.Errorf("failed to load TLS certificate: %w", err)
 	}
 
 	config := warp.ServerConfig{
-		Addr:   *addr,
+		Addr:   myConfig.address,
 		Cert:   &tlsCert,
-		LogDir: *logDir,
+		LogDir: myConfig.logDir,
 	}
 
 	ws, err := warp.NewServer(config, media)
@@ -49,7 +52,52 @@ func run(ctx context.Context) (err error) {
 		return fmt.Errorf("failed to create warp server: %w", err)
 	}
 
-	log.Printf("listening on %s", *addr)
+	log.Printf("listening on %s", myConfig.address)
 
 	return invoker.Run(ctx, invoker.Interrupt, ws.Run)
+}
+
+func getConfig() Config {
+	//TODO: support config files
+	addr := flag.String("port", ":8080", "HTTPS server address")
+	cert := flag.String("tls-cert", "./certs/localhost.crt", "TLS certificate file path")
+	key := flag.String("tls-key", "./certs/localhost.key", "TLS certificate file path")
+	logDir := flag.String("log-dir", "", "logs will be written to the provided directory")
+	dash := flag.String("media", "./media/playlist.mpd", "DASH playlist path")
+
+	flag.Parse()
+
+	conf := Config{
+		logDir: *logDir,
+	}
+
+	//get port
+	if *addr == "" {
+		conf.address = os.Getenv("WARP_PORT")
+	} else {
+		conf.address = *addr
+	}
+
+	//get certFile
+	if *cert == "" {
+		conf.certFile = os.Getenv("WARP_CERTFILE")
+	} else {
+		conf.certFile = *cert
+	}
+
+	//get keyFile
+	if *key == "" {
+		conf.keyFile = os.Getenv("WARP_KEYFILE")
+	} else {
+		conf.keyFile = *key
+	}
+
+	//get media folder
+	if *dash == "" {
+		conf.media = os.Getenv("WARP_MEDIA")
+	} else {
+		conf.media = *dash
+	}
+
+	return conf
 }
