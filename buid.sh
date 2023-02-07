f894224f337f908a5bf5dea17eb324691bb2aac6
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-02-07 21:31:47 +0300

    new test(s)

---
+++ buid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+#copy dockerfile into the root of the directory
+cp .docker/base/Dockerfile .
+
+#build and run docker
+docker build -t wanjohiryan/warp:x264 .
+
+#on build remove the Dockerfile, in case we create an 'example'
+rm Dockerfile 
+
+set -e
+#run the image for 4 minutes
+timeout 4m docker run wanjohiryan/warp:x264
