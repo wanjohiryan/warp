671c6391b0c0286e59ff7558f49235086719b0fe
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-02-10 02:21:22 +0300

    tini

--- build-example.sh
+++ build-example.sh
@@ -2,17 +2,6 @@
 
 set -xeo pipefail
 
-f(){
-  errorCode=$?
-  echo "error $errorCode"
-  echo "$BASH_COMMAND"
-  echo "on line ${BASH_LINENO[0]}"
-
-  exit $errorCode
-}
-
-trap -f ERR
-
 #copy dockerfile into the root of the directory
 cp examples/firefox/Dockerfile .
 
@@ -24,8 +13,6 @@ rm Dockerfile
 
 #run the image for 4 minutes
 docker run wanjohiryan/warp:firefox-x264 &
-
-false
 
 sleep 20
 kill %?docker
