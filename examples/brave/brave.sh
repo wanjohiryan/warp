d7b6bcbd6c23eb0e4c3c1182cc7e9bcceedecc70
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-02-17 09:08:01 +0300

    user data directory

--- examples/brave/brave.sh
+++ examples/brave/brave.sh
@@ -2,19 +2,12 @@
 
 set -xeo pipefail
 
-trap 'trap " " SIGINT; kill -SIGINT 0; wait;' SIGINT SIGTERM
-
-YOUTUBE_URL="https://www.youtube.com/watch?v=mCCOz1dUz-Q"
 SCREEN_WIDTH=1920
 SCREEN_HEIGHT=1080
 
-#Run the base entrypoint and wait for 10 seconds
-source /etc/warp/entrypoint.sh &
-sleep 10
-
 #Create brave user directory
-USER_DATA_DIR="/home/user/.config/brave-browser"
-mkdir -p "$USER_DATA_DIR"
+USER_DATA_DIR="/home/$USER/.config/brave-browser"
+sudo mkdir -p "$USER_DATA_DIR"
 
 # Start Brave browser and play kygo
 brave-browser \
