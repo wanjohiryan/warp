ef1a40474ec0a9db9e0cec47c15155ff57064c30
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-03-11 02:36:56 +0300

    Fix egl compatibility

--- docker/nvidia/entrypoint.sh
+++ docker/nvidia/entrypoint.sh
@@ -13,90 +13,22 @@ sudo chmod 700 /tmp/runtime-user
 
 # Remove directories to make sure the desktop environment starts
 sudo rm -rf /tmp/.X* ~/.cache
-# This symbolic link enables running Xorg inside a container with `-sharevts`
-sudo ln -snf /dev/ptmx /dev/tty7
 
 #Start dbus for pulseaudio
 sudo /etc/init.d/dbus start
 
-# Install NVIDIA userspace driver components including X graphic libraries
-if ! command -v nvidia-xconfig &> /dev/null; then
-  # Driver version is provided by the kernel through the container toolkit
-  export DRIVER_VERSION=$(head -n1 </proc/driver/nvidia/version | awk '{print $8}')
-  cd /tmp
-  # If version is different, new installer will overwrite the existing components
-  if [ ! -f "/tmp/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" ]; then
-    # Check multiple sources in order to probe both consumer and datacenter driver versions
-    curl -fsL -O "https://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" || curl -fsL -O "https://us.download.nvidia.com/tesla/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" || { echo "Failed NVIDIA GPU driver download. Exiting."; exit 1; }
-  fi
-  # Extract installer before installing
-  sudo sh "NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" -x
-  cd "NVIDIA-Linux-x86_64-$DRIVER_VERSION"
-  # Run installation without the kernel modules and host components
-  sudo ./nvidia-installer --silent \
-                    --no-kernel-module \
-                    --install-compat32-libs \
-                    --no-nouveau-check \
-                    --no-nvidia-modprobe \
-                    --no-rpms \
-                    --no-backup \
-                    --no-check-for-alternate-installs
-  sudo rm -rf /tmp/NVIDIA* && cd ~
-fi
+# Change time zone from environment variable
+sudo ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" | sudo tee /etc/timezone > /dev/null
 
-# Allow starting Xorg from a pseudoterminal instead of strictly on a tty console
-if [ ! -f /etc/X11/Xwrapper.config ]; then
-    echo -e "allowed_users=anybody\nneeds_root_rights=yes" | sudo tee /etc/X11/Xwrapper.config > /dev/null
-fi
-if grep -Fxq "allowed_users=console" /etc/X11/Xwrapper.config; then
-  sudo sed -i "s/allowed_users=console/allowed_users=anybody/;$ a needs_root_rights=yes" /etc/X11/Xwrapper.config
-fi
+# Add game directories for VirtualGL directories to path
+export PATH="${PATH}:/usr/games:/opt/VirtualGL/bin"
 
-# Remove existing Xorg configuration
-if [ -f "/etc/X11/xorg.conf" ]; then
-  sudo rm -f "/etc/X11/xorg.conf"
-fi
+# Add CUDA library path
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:${LD_LIBRARY_PATH}"
 
-# Get first GPU device if all devices are available or `NVIDIA_VISIBLE_DEVICES` is not set
-if [ "$NVIDIA_VISIBLE_DEVICES" == "all" ]; then
-  export GPU_SELECT=$(sudo nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)
-elif [ -z "$NVIDIA_VISIBLE_DEVICES" ]; then
-  export GPU_SELECT=$(sudo nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)
-# Get first GPU device out of the visible devices in other situations
-else
-  export GPU_SELECT=$(sudo nvidia-smi --id=$(echo "$NVIDIA_VISIBLE_DEVICES" | cut -d ',' -f1) --query-gpu=uuid --format=csv | sed -n 2p)
-  if [ -z "$GPU_SELECT" ]; then
-    export GPU_SELECT=$(sudo nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)
-  fi
-fi
-
-if [ -z "$GPU_SELECT" ]; then
-  echo "No NVIDIA GPUs detected or nvidia-container-toolkit not configured. Exiting."
-  exit 1
-fi
-
-# Bus ID from nvidia-smi is in hexadecimal format, should be converted to decimal format which Xorg understands, required because nvidia-xconfig doesn't work as intended in a container
-HEX_ID=$(sudo nvidia-smi --query-gpu=pci.bus_id --id="$GPU_SELECT" --format=csv | sed -n 2p)
-IFS=":." ARR_ID=($HEX_ID)
-unset IFS
-BUS_ID=PCI:$((16#${ARR_ID[1]})):$((16#${ARR_ID[2]})):$((16#${ARR_ID[3]}))
-# A custom modeline should be generated because there is no monitor to fetch this information normally
-export MODELINE=$(cvt -r "${SIZEW}" "${SIZEH}" "${REFRESH}" | sed -n 2p)
-# Generate /etc/X11/xorg.conf with nvidia-xconfig
-sudo nvidia-xconfig --virtual="${SIZEW}x${SIZEH}" --depth="$CDEPTH" --mode=$(echo "$MODELINE" | awk '{print $2}' | tr -d '"') --allow-empty-initial-configuration --no-probe-all-gpus --busid="$BUS_ID" --no-multigpu --no-sli --no-base-mosaic --only-one-x-screen ${CONNECTED_MONITOR}
-# Guarantee that the X server starts without a monitor by adding more options to the configuration
-sudo sed -i '/Driver\s\+"nvidia"/a\    Option         "ModeValidation" "NoMaxPClkCheck, NoEdidMaxPClkCheck, NoMaxSizeCheck, NoHorizSyncCheck, NoVertRefreshCheck, NoVirtualSizeCheck, NoExtendedGpuCapabilitiesCheck, NoTotalSizeCheck, NoDualLinkDVICheck, NoDisplayPortBandwidthCheck, AllowNon3DVisionModes, AllowNonHDMI3DModes, AllowNonEdidModes, NoEdidHDMI2Check, AllowDpInterlaced"\n    Option         "HardDPMS" "False"' /etc/X11/xorg.conf
-# Add custom generated modeline to the configuration
-sudo sed -i '/Section\s\+"Monitor"/a\    '"$MODELINE" /etc/X11/xorg.conf
-# Prevent interference between GPUs, add this to the host or other containers running Xorg as well
-echo -e "Section \"ServerFlags\"\n    Option \"AutoAddGPU\" \"false\"\nEndSection" | sudo tee -a /etc/X11/xorg.conf > /dev/null
-
-#RUN x11 virtual framebuffer
-SCREEN_RESOLUTION=1920x1080
-DPI=96
-# Run Xorg server with required extensions
-Xorg vt7 -noreset -novtswitch -sharevts -dpi "${DPI}" +extension "GLX" +extension "RANDR" +extension "RENDER" +extension "MIT-SHM" "${DISPLAY}" &
-sleep 0.5 #ensure this has started before moving on
+# Run Xvfb server with required extensions
+Xvfb "${DISPLAY}" -ac -screen "0" "8192x4096x24" -dpi "96" +extension "RANDR" +extension "GLX" +iglx +extension "MIT-SHM" +render -nolisten "tcp" -noreset -shmem &
+sleep 0.5
 
 # Wait for X11 to start
 echo "Waiting for X socket"
