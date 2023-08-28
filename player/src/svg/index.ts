94b29e1d7a26f9fbe95e0c54b3b6b84bfd9a3114
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-08-28 11:35:14 +0300

    rm

--- player/src/svg/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Gpad} from "./gpad"
-export {default as Logo} from "./logo"
-export {default as LogoName} from "./logoName"
--- player/src/transport/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as MediaTransport} from "./mediaTransport"
-export {default as InputTransport} from "./inputTransport"
